e054a0d1fb4cf974992813bf3f22e4dbc07cb807
magdasaif <magdasaif3@gmail.com>
2025-06-23 01:56:28 -0700

    add tabs for date  filteration in countries list page

--- app/Filament/Resources/CountryResource/Pages/ListCountries.php
+++ app/Filament/Resources/CountryResource/Pages/ListCountries.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\CountryResource\Pages;
 
 use App\Filament\Resources\CountryResource;
+use App\Models\Country;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCountries extends ListRecords
@@ -14,6 +16,16 @@ class ListCountries extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All'           => Tab::make()->badge(Country::count()) ->icon('heroicon-o-flag'),
+            'This Week'     => Tab::make()->query(fn ($query) => $query->where('created_at', '>=', now()->subWeek()))->badge(Country::query()->where('created_at', '>=', now()->subWeek())->count()),
+            'This Month'    => Tab::make()->query(fn ($query) => $query->where('created_at', '>=', now()->subMonth()))->badge(Country::query()->where('created_at', '>=', now()->subMonth())->count()),
+            'This Year'     => Tab::make()->query(fn ($query) => $query->where('created_at', '>=', now()->subYear()))->badge(Country::query()->where('created_at', '>=', now()->subYear())->count()),
         ];
     }
 }
